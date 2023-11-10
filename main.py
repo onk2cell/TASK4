8d573274cc9fca6aa046311619ff7cb8d6d3d558
onk2cell <116635851+onk2cell@users.noreply.github.com>
2023-11-10 11:15:53 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,171 @@
+
+import streamlit as st
+import os
+from decouple import config
+from PyPDF2 import PdfReader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.chains import RetrievalQA
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Import necessary libraries
+import streamlit as st
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationEntityMemory
+from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
+from langchain.llms import OpenAI
+
+# Set Streamlit page configuration
+st.set_page_config(page_title='RAG + MEMOREY BOT', layout='wide')
+
+uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+api_key = st.text_input("Enter the OpenAI API key")
+if uploaded_file is not None and api_key:
+    # Read the uploaded PDF file
+    doc_reader = PdfReader(uploaded_file)
+
+    raw_text = ''
+    for i, page in enumerate(doc_reader.pages):
+        text = page.extract_text()
+        if text:
+            raw_text += text
+
+    # Splitting up the text into smaller chunks for indexing
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len,
+    )
+    texts = text_splitter.split_text(raw_text)
+    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
+    docsearch = FAISS.from_texts(texts, embeddings)
+
+
+
+# Initialize session states
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
+if "past" not in st.session_state:
+    st.session_state["past"] = []
+if "input" not in st.session_state:
+    st.session_state["input"] = ""
+if "stored_session" not in st.session_state:
+    st.session_state["stored_session"] = []
+
+# Define function to get user input
+def get_text():
+    """
+    Get the user input text.
+
+    Returns:
+        (str): The text entered by the user
+    """
+    input_text = st.text_input("You: ", st.session_state["input"], key="input",
+                            placeholder="Your AI assistant here! Ask me anything ...", 
+                            label_visibility='hidden')
+    return input_text
+
+# Define function to start a new chat
+def new_chat():
+    """
+    Clears session state and starts a new chat.
+    """
+    save = []
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        save.append("User:" + st.session_state["past"][i])
+        save.append("Bot:" + st.session_state["generated"][i])        
+    st.session_state["stored_session"].append(save)
+    st.session_state["generated"] = []
+    st.session_state["past"] = []
+    st.session_state["input"] = ""
+    st.session_state.entity_memory.entity_store = {}
+    st.session_state.entity_memory.buffer.clear()
+
+# Set up sidebar with various options
+with st.sidebar.expander("🛠️ ", expanded=False):
+    # Option to preview memory store
+    if st.checkbox("Preview memory store"):
+        with st.expander("Memory-Store", expanded=False):
+            st.session_state.entity_memory.store
+    # Option to preview memory buffer
+    if st.checkbox("Preview memory buffer"):
+        with st.expander("Bufffer-Store", expanded=False):
+            st.session_state.entity_memory.buffer
+    MODEL = st.selectbox(label='Model', options=['gpt-3.5-turbo','text-davinci-003','text-davinci-002','code-davinci-002'])
+    K = st.number_input(' (#)Summary of prompts to consider',min_value=3,max_value=1000)
+
+# Set up the Streamlit app layout
+st.title("CHAT WITH BOT")
+st.subheader("******")
+
+# Ask the user to enter their OpenAI API key
+API_O = st.sidebar.text_input("API-KEY", type="password")
+
+# Session state storage would be ideal
+if API_O:
+    # Create an OpenAI instance
+    llm = OpenAI(temperature=0,
+                openai_api_key=API_O, 
+                model_name=MODEL, 
+                verbose=False) 
+
+
+    # Create a ConversationEntityMemory object if not already created
+    if 'entity_memory' not in st.session_state:
+            st.session_state.entity_memory = ConversationEntityMemory(llm=llm, k=K )
+        
+        # Create the ConversationChain object with the specified configuration
+    Conversation = ConversationChain(
+            llm=llm, 
+            prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+            memory=st.session_state.entity_memory
+        )  
+else:
+    st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
+    # st.stop()
+
+
+# Add a button to start a new chat
+st.sidebar.button("New Chat", on_click = new_chat, type='primary')
+
+# Get the user input
+user_input = get_text()
+
+# Generate the output using the ConversationChain object and the user input, and add the input/output to the session
+if user_input:
+    docs = docsearch.similarity_search(user_input, k=5)
+    output = Conversation.run(input=user_input+str(docs))
+    print(output)  #JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+    st.session_state.past.append(user_input)  
+    st.session_state.generated.append(output)  
+
+# Allow to download as well
+download_str = []
+# Display the conversation history using an expander, and allow the user to download it
+with st.expander("Conversation", expanded=True):
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        st.info(st.session_state["past"][i],icon="")
+        st.success(st.session_state["generated"][i], icon="")
+        download_str.append(st.session_state["past"][i])
+        download_str.append(st.session_state["generated"][i])
+    
+    # Can throw error - requires fix
+    download_str = '\n'.join(download_str)
+    if download_str:
+        st.download_button('Download',download_str)
+
+# Display stored conversation sessions in the sidebar
+for i, sublist in enumerate(st.session_state.stored_session):
+        with st.sidebar.expander(label= f"Conversation-Session:{i}"):
+            st.write(sublist)
+
+# Allow the user to clear all stored conversation sessions
+if st.session_state.stored_session:   
+    if st.sidebar.checkbox("Clear-all"):
+        del st.session_state.stored_session
